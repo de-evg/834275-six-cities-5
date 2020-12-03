207bea18135c55b8e3311530b969fc89e829d424
de-evg <d.e.min@yandex.ru>
2020-12-03 17:17:47 +0300

    created first components

---
+++ src/components/main-menu-item/main-menu-item.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MainMenuItem = () => {
+  return (
+    <li className="locations__item">
+      <a className="locations__item-link tabs__item" href="#">
+        <span>Paris</span>
+      </a>
+    </li>
+  );
+};
+
+export default MainMenuItem;
---
+++ src/components/main-menu-list/main-menu-list.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainMenuList = () => {
+  return (
+    <ul className="locations__list tabs__list"></ul>
+  );
+};
+
+export default MainMenuList;
